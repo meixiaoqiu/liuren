e139fb9530dca8c0b6034f60fc75013e34a5c10f
MXQ <meixiaoqiu@gmail.com>
2014-01-21 23:33:52 +0800

    重构

--- com2.js
+++ com2.js
@@ -1,22 +1,156 @@
-var _firstYear=4; //第一个甲子年为公元4年
-var _earth=[0,1,2,3,4,5,6,7,8,9,10,11]; //十二地支
-var _earthStr=["子","丑","寅","卯","辰","巳","午","未","申","酉","戌","亥"];
-var _sky=[0,1,2,3,4,5,6,7,8,9]; //十天干
-var _skyStr=["甲","乙","丙","丁","戊","己","庚","辛","壬","癸"];
-var _solarTerm=["春分","清明","谷雨","立夏","小满","芒种","夏至","小暑","大暑","立秋","处暑","白露","秋分","寒露","霜降","立冬","小雪","大雪","冬至","小寒","大寒","立春","雨水","惊蛰"];
-var _monthGeneral=[1,0,11,10,9,8,7,6,5,4,3,2,1,0]; //月将，从冬至之后第一个月将开始。因为算出的中
-var _monthGeneralStr=["丑","子","亥","戌","酉","申","未","午","巳","辰","卯","寅","丑","子"]; //月将，从冬至之后第一个月将开始。因为算出的中气数组可能有14个，所以月将也为14个
+var _jiaZi=4; //第一个甲子年为公元4年
+var _diZhi=[0,1,2,3,4,5,6,7,8,9,10,11]; //十二地支
+var _diZhiStr=["子","丑","寅","卯","辰","巳","午","未","申","酉","戌","亥"];
+var _tianGan=[0,1,2,3,4,5,6,7,8,9]; //十天干
+var _tianGanStr=["甲","乙","丙","丁","戊","己","庚","辛","壬","癸"];
+var _jieQi=["春分","清明","谷雨","立夏","小满","芒种","夏至","小暑","大暑","立秋","处暑","白露","秋分","寒露","霜降","立冬","小雪","大雪","冬至","小寒","大寒","立春","雨水","惊蛰"];
+var _yueJiang=[1,0,11,10,9,8,7,6,5,4,3,2,1,0];//月将，从冬至之后第一个月将开始。因为算出的中
+var _yueJiangStr=["丑","子","亥","戌","酉","申","未","午","巳","辰","卯","寅","丑","子"];//月将，从冬至之后第一个月将开始。因为算出的中气数组可能有14个，所以月将也为14个
 var _jiGong=[2,4,5,7,5,7,8,10,11,1]; //十干寄宫
 var _jiGongZhi=[-1];
-var _five=[0,1,2,3,4];
-var _fiveStr=["金","木","土","水","火"];
-var _earthFive=[3,2,1,1,2,4,4,2,0,0,2,3]; //地支五行
-var _skyFive=[1,1,4,4,2,2,0,0,3,3]; //天干五行
-var _earthYinYang=[1,0,1,0,1,0,1,0,1,0,1,0]; //地支阴阳 0阴 1阳
-var _skyYinYang=[1,0,1,0,1,0,1,0,1,0]; //天干阴阳
+var _wuXing=[0,1,2,3,4];
+var _wuXingStr=["金","木","土","水","火"];
+var _diZhiWuXing=[3,2,1,1,2,4,4,2,0,0,2,3]; //地支五行
+var _tianGanWuXing=[1,1,4,4,2,2,0,0,3,3]; //天干五行
+var _diZhiYinYang=[1,0,1,0,1,0,1,0,1,0,1,0]; //地支阴阳 0阴 1阳
+var _tianGanYinYang=[1,0,1,0,1,0,1,0,1,0]; //天干阴阳
 var _siMeng=[2,5,8,11]; //四孟 寅申巳亥
 var _siZhong=[0,3,6,9]; //四仲 子午卯酉
 var _siJi=[1,4,7,10]; //四季 辰戌丑未
+
+
+//卦
+var g={
+	siZhu:[],
+	yueJiang,
+	tianpan:[],
+	siKe:[],
+	chuChuan,
+	zhongChuan,
+	moChuan,
+	
+	init:function(siZhu,yueJiang){
+		this.siZhu=siZhu;
+		this.yueJiang=yueJiang;
+	},
+	
+	//月将加时排天地盘
+	tianDiPan:function(){
+		var shiZhi=this.siZhu[7];
+		var yueJiang=this.yueJiang;
+		var tianPan=[];
+		var tianPanFirst;
+		if(shiZhi<=yueJiang){
+			tianPanFirst=yueJiang-shiZhi;
+		}else{
+			tianPanFirst=yueJiang+12-shiZhi;
+		}
+		//循环12次排出天盘
+		var j=0;
+		for(i=0;i<=11;i++){
+			tianPan[i]=tianPanFirst+i;
+			if(tianPan[i]>11){
+				tianPan[i]=j;
+				j++;
+			}
+		}
+		this.tianPan=tianPan;
+	},
+	
+	//排四课
+	function siKe(){
+		var riGan=this.siZhu[4];
+		var riZhi=this.siZhu[5];
+		var tianPan=this.tianPan;
+		//第一课
+		var ke1=[];
+		ke1[0]=riGan;
+		riGanJiGong=jiGong(riGan);
+		ke1[1]=tianPan[riGanJiGong];
+		
+		//第二课
+		var ke2=[];
+		ke2[0]=ke1[1];
+		ke2[1]=tianPan[ke2[0]];
+		
+		//第三课
+		var ke3=[];
+		ke3[0]=riZhi;
+		ke3[1]=tianPan[ke3[0]];
+		
+		//第四课
+		var ke4=[];
+		ke4[0]=ke3[1];
+		ke4[1]=tianPan[ke4[0]];
+		this.siKe= [ke1[0],ke1[1],ke2[0],ke2[1],ke3[0],ke3[1],ke4[0],ke4[1]];
+	},
+	
+	//排三传
+	function sanChuan(){
+		var siKe=this.siKe;
+		var tianPan=this.tianPan;
+		
+		//循环四课的八个值，得出五行值
+		var siKeWuXing=[];
+		for(i=0;i<siKe.length;i++){
+			if(i==0){
+				siKeWuXing[i]=_skyFive[siKe[i]];
+			}else{
+				siKeWuXing[i]=_earthFive[siKe[i]];
+			}
+		}
+		
+		//四课的五行生克关系
+		var siKeShengKe=[];
+		var j=0;
+		for(i=0;i<siKeWuXing.length;i++){
+			if((i+2)%2==0){
+				siKeShengKe[j]=shengKe(siKeWuXing[i],siKeWuXing[i+1]);
+				j++;
+			}
+		}
+		
+		var keArr=[]; //上克下索引
+		var zeiArr=[]; //下贼上索引
+		for(i=0;i<siKeShengKe.length;i++) {
+			if(siKeShengKe[i]==-1){
+				keArr.push(i);
+			}
+			if(siKeShengKe[i]==1){
+				zeiArr.push(i);
+			}
+		}
+		
+		var r=
+		
+		
+	}
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //根据年份参数返回60花甲年
 function huaJia(year){
